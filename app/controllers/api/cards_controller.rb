befce02791d6a9c39cebcfef1f20aa9580d216b9
Kevin Hurley <kphurley@meraki.com>
2022-08-12 09:07:50 -0500

    Add cardgamedb card images to card pages

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -1,7 +1,9 @@
 class Api::CardsController < ApplicationController
   def show
+    card = Card.find(params[:id])
+
     respond_to do |format|
-      format.json { render json: Card.find(params[:id]) }
+      format.json { render json: card.attributes.merge({ set: card.card_block.card_set }) }
     end
   end
 end
--- app/javascript/pages/Card.jsx
+++ app/javascript/pages/Card.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import CardInfo from "../components/CardPanel";
+import getCardImageUrl from "../util/getCardImageUrl";
 
 // List a description of the card with it's image
 const Card = () => {
@@ -18,12 +19,16 @@ const Card = () => {
 
   return (
     <div className="container">
-      <div className="col-md-12">
-        <div className="col-sm-6 col-sm-offset-1">
-          <CardInfo includeFlavorText cardData={cardData} />
-        </div>
-        <div className="col-sm-4">
-          {/* image here */}
+      <div className="row">
+        <div className="col-md-12">
+          <div className="row">
+            <div className="col-sm-6 col-sm-offset-1">
+              <CardInfo includeFlavorText cardData={cardData} />
+            </div>
+            <div className="col-sm-4">
+              { cardData.set && <img src={ getCardImageUrl(cardData) }></img> }
+            </div>
+          </div>
         </div>
       </div>
     </div>
