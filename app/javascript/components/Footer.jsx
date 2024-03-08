7645b76d56ff3f4b2adf2a45cf6c95c5434126e9
Kevin Hurley <kphurley@meraki.com>
2024-03-08 17:12:29 -0600

    Implement message in footer with github and patreon links

--- app/javascript/components/Footer.jsx
+++ app/javascript/components/Footer.jsx
@@ -3,11 +3,23 @@ import * as React from "react";
 const Footer = () =>
   <div className="container">
     <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-      <p className="col-md-4 mb-0 text-muted">© 2024 Kevin Hurley</p>
+      <p>© 2024 Kevin Hurley</p>
 
-      <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-        <svg className="bi me-2" width="40" height="32"><use xlinkHref="#bootstrap"></use></svg>
-      </a>
+      <p>
+        Hey!  Thanks so much for stopping by and checking out swlcgdb.com.
+        This application is considered an ALPHA release.  As in - things likely WILL break as you try to do things.
+
+        Please report any bugs you find to <a href="https://github.com/kphurley/swlcgdb/issues">Github</a>.
+      </p>
+
+      <p>
+        If you appreciate the work done here to create this resource, please consider donating to <a href="https://www.patreon.com/kphurley">my patreon</a>.
+        I promise every cent donated there will go towards the cost to keep the lights on for the site.
+      </p>
+
+      <p>
+        Thanks, and enjoy!  --Kevin
+      </p>
     </footer>
   </div>;
 
